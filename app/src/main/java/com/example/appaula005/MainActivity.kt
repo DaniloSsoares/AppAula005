014425b2dc04fbb8b269b50149795d4e718955fd
DaniloSsoares <danilo.ssoares201@gmail.com>
2023-03-22 11:13:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appaula005/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.appaula005
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val edtNome: EditText = findViewById(R.id.text)
+        val btnCadastrar: Button = findViewById(R.id.btnCadastrar)
+        btnCadastrar.setOnClickListener{
+            val toast = Toast.makeText(this, edtNome.text.toString(), Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+}
